72b5ceec5959bafb64551f8e549ec58bf4096623
nithincod <nithinrohitian@gmail.com>
2024-04-25 22:36:18 +0530

    React code done

---
+++ src/comp/footer.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import './footer.css'
+import { FaPiggyBank, FaShippingFast, FaHeadphonesAlt, FaWallet} from 'react-icons/fa';
+const Footer = () => {
+  return (
+    <>
+    <div className='footer'>
+        <div className='container'>
+            <div className='left-box'>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaPiggyBank />
+                    </div>
+                    <div className='detail'>
+                        <h3>Great Savings</h3>
+                        <p>You have Great savings on all products</p>
+                    </div>
+                </div>
+                    <div className='box'>
+                    <div className='icon_box'>
+                        <FaShippingFast />
+                    </div>
+                    <div className='detail'>
+                        <h3>free delivery</h3>
+                        <p>Free delivery available for selected areas.</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaHeadphonesAlt />
+                    </div>
+                    <div className='detail'>
+                        <h3>24X7 support</h3>
+                        <p>No tension we are 24/7 active</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaWallet />
+                    </div>
+                    <div className='detail'>
+                        <h3>money back</h3>
+                        <p>dont worry we have return money feature.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='right_box'>
+                <div className='header'>
+                    <img src='image/innov.jpg' width={80}alt=''></img>
+                    <p>Any gadgets any time any where only Innov8tive</p>
+                </div>
+                <div className='bottom'>
+                    <div className='box'>
+                        <h3>Your Account</h3>
+                        <ul>
+                            <li>About us</li>
+                            <li>Account</li>
+                            <li>Payment</li>
+                            <li>sales</li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <h3>products</h3>
+                        <ul>
+                            <li>Delivery</li>
+                            <li>Track Oder</li>
+                            <li>New product</li>
+                            <li>old product</li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <h3>contact us</h3>
+                        <ul>
+                            <li>Hitech city,Hyderbad</li>
+                            <li>+91 9030232240</li>
+                            <li>INNOV8TIVE@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer
